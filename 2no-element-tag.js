891f7724a89ae2a68f7c418b94e41fb0db232d15
MirzaAyon <ayonmirza98@gmail.com>
2022-07-22 23:12:05 +0600

    first commit

---
+++ 2no-element-tag.js
@@ -0,0 +1,78 @@
+//shopping cart open - caching er + e inspact - dekha jabe i ache ekta btn ache - but click korle je maan bartese tokhn bujha jache na ekhane event listener ache kina
+//tai case-plus id take copy kore nilam
+//js file e giye conrl+f marle search option ashbe okhane copy + enter dile je je place e ache shob chole ashbe
+//jodi js file onk gula thake tkhn control+shift+f marbo tahole shob file gula check hoye je je place e ache shob gula show hbe
+//evebe bujhte parbo kothae kothae event listener ache
+//inspact e layout er pashe event listener e click korle bole dibe ekhane ekta click handler ache
+//click ke expand korle dekha jabe app.js er line no koto te ache ta bole dibe aro info dibe , line no 47 e ache , line no e click korle source tab e chole jabe jekhane shob code ache and 47 no line show korbe
+//element er btn e right click korle onk kichue korar option ashbe duplicate option e click korle button 2 ta bananor option ashbe
+//delete element, edit html shob e kora jabe
+//erpr new take delete kore dbo
+//hide element,copy,paste shob e use kora jae
+//erpr ekta option ache dom breakpoint name
+//jodi breakpoint thake tkhn show hbe
+//na thakle show hbe na
+// btn e right click - break on - break on suntree modifications dibo - dom breakpoint e ta show hbe - kothae kothae breakpoint ache ta dekhte pabo
+//acharao property name ekta option ache otao chaile dekhte pabo - ki ki element,node,html ache shob dekhte pabo
+//erpr dekhbo accessibility
+//google - accessibility
+
+//input e giye input tag e right click - copy - copy styles ekhn copputed er joto css style ache shb copy hye gelo
+
+/*
+    --bs-blue: #0d6efd;
+    --bs-indigo: #6610f2;
+    --bs-purple: #6f42c1;
+    --bs-pink: #d63384;
+    --bs-red: #dc3545;
+    --bs-orange: #fd7e14;
+    --bs-yellow: #ffc107;
+    --bs-green: #198754;
+    --bs-teal: #20c997;
+    --bs-cyan: #0dcaf0;
+    --bs-white: #fff;
+    --bs-gray: #6c757d;
+    --bs-gray-dark: #343a40;
+    --bs-primary: #0d6efd;
+    --bs-secondary: #6c757d;
+    --bs-success: #198754;
+    --bs-info: #0dcaf0;
+    --bs-warning: #ffc107;
+    --bs-danger: #dc3545;
+    --bs-light: #f8f9fa;
+    --bs-dark: #212529;
+    --bs-font-sans-serif: system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    --bs-font-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+    --bs-gradient: linear-gradient(180deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0));
+    -webkit-text-size-adjust: 100%;
+    -webkit-tap-highlight-color: transparent;
+    --bs-gutter-x: 1.5rem;
+    --bs-gutter-y: 0;
+    box-sizing: border-box;
+    margin: 0;
+    font-family: inherit;
+    display: block;
+    padding: .375rem .75rem;
+    line-height: 1.5;
+    color: #212529;
+    background-clip: padding-box;
+    appearance: none;
+    transition: none;
+    text-align: center!important;
+    position: relative;
+    flex: 1 1 auto;
+    min-width: 0;
+    background-color: #F6F5FA;
+    border: none;
+    width: 65px;
+    border-radius: 10px !important;
+    font-weight: 700;
+    font-size: 20px;
+    margin-left: -1px;
+*/
+// ei full style chaile kno website e use krte parbo
+
+//inpact er oi place e contrl+f - .col-md-5 dile eke eke show hbe
+//upore niche gele shob show hbe
+//.col-md-5 button dile under e er shob btn show hbe
+//how to make a website accessible? - google 10 ta ache
---
+++ devtool-shortcut.js
@@ -0,0 +1,15 @@
+//elements er ekdom pura baame ja ache ota picker ja dara select kora jae
+//r bamer ta holo device er jnno
+//element er daner style,compound,layout agei dkhsi
+
+//elements,console,source,network,memory,application,security,lighthouse,settings egula aste aste shikhbo
+
+
+//normally inspact - console - console.log(20) dile kichu run hbe and reload dile chole jabe
+//but settings-console er preserve log upon navigation dile reload dile r jabe na console ta theke jabe
+//shob API ke hit korar jnno - console er log XML https request eta on korte hbe
+//settings e aro onk device on kore rakha jae
+//settings - e loction dekhbo
+//shortcuts dekhbo
+//shortcut er niche ache full list of devtool shortcut er link
+//upore click korle devtool shortcut vs shortcut 2 tai dekha jabe
