3a615f77d03841dcf679fd95b66e9a6ab247d39f
MirzaAyon <ayonmirza98@gmail.com>
2022-07-23 12:16:21 +0600

    console tab done

---
+++ 3no-consoletab.js
@@ -0,0 +1,91 @@
+// document.getElementById('phone-plus')
+// <button id=​"phone-plus" class=​"btn btn-default">​…​</button>
+// document.querySelector
+// ƒ querySelector() { [native code] }
+
+// $('#phone-plus')
+// <button id=​"phone-plus" class=​"btn btn-default">​…​</button>
+//eta jquery er moto query selector er shortcut eta holo css selector tai hash dilam
+
+
+
+// console.log
+//software engineering log labels - google 10/12 ta ache shob dekhbo
+//inspact e console.log
+//dekhbo
+//erpr console. likhe suggestions gula dekhbo
+
+
+
+// console.info
+// ƒ info() { [native code] }
+// console.info('regular info')
+// VM690:1 regular info
+// undefined
+// console.log('regular log')
+// VM758:1 regular log
+// undefined
+// console.warn('dont come')
+// VM847:1 dont come
+// (anonymous) @ VM847:1
+// undefined
+
+// console.error('error')
+// VM923:1 error
+// (anonymous) @ VM923:1
+// undefined
+
+//clear() dile shob clr hye jabe
+//console e
+
+
+
+//special func ke monitoring korar upay
+//eta randomly korle hbe na amr banano kono site er inspact e giye krte hbe
+// monitor('updateProductNumber')
+// undefined
+// monitor('updateProductNumber)
+// VM152:1 Uncaught SyntaxError: Invalid or unexpected token
+// monitor(updateProductNumber)
+// undefined
+// VM180:1 function updateProductNumber called with arguments: case, 59, true
+// VM181:1 function updateProductNumber called with arguments: case, 59, false
+// unmonitor(updateProductNumber) //monitor off hbe
+// undefined
+
+
+
+
+
+// const phoneplus = $('#phone-plus')
+// undefined
+// phoneplus
+// <button id=​"phone-plus" class=​"btn btn-default">​…​</button>
+// phoneplus
+// <button id=​"phone-plus" class=​"btn btn-default">​…​</button>
+// phoneplus
+// <button id=​"phone-plus" class=​"btn btn-default">​…​</button>
+// monitorEvents(phoneplus)
+// undefined
+//mouse diye click kore dekhbo
+
+
+
+
+// unmonitor(phoneplus)
+// undefined
+// monitorEvents
+// ƒ monitorEvents() { [native code] }
+// monitorEvents(phoneplus,['click', 'mouseout'])
+// undefined
+// mouse diye click kore dekhbo
+
+
+
+// ekta func er vitore console.count('clicked');
+// ei typer jinish likhle console e dekhabe ami koto bar click kortesi ta
+
+//chorme devtoop console api - google kore eta shikhbo
+
+// console e copy(30)
+// erpr cnntrol+v dile copy hye jabe
