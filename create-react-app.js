b6942a377794c3d9ba1bc0a512e853d68ec8128b
MirzaAyon <ayonmirza98@gmail.com>
2022-07-23 20:38:19 +0600

    quiz and react app installation added

---
+++ create-react-app.js
@@ -0,0 +1,149 @@
+//google - create react app - react or
+// npx create-react-app my-app
+// cd my-app
+// npm start
+//ei jinish ta lgbe
+//beshi time nile control c mere stop kora jabe
+//eta git link ekhane geleo same jnish pabo
+//https://github.com/facebook/create-react-app
+//abr ekhane geleo same jinish pabo
+// https://create-react-app.dev/
+//docs e gelei pabo
+//cd mane change directory
+//laptop er disk mane compact disk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//debug revision
+//console e default levels ache kichu
+//chaile thik kore dewa jabe error,warning,info egular modhe konta dekhte chai na chai
+
+
+
+
+
+//full installation
+Microsoft Windows[Version 10.0.19044.1826]
+(c) Microsoft Corporation.All rights reserved.
+
+    C: \Users\pc > cd projects
+The system cannot find the path specified.
+
+    C: \Users\pc > cd / projects
+
+C: \projects > cd npx create - react - app my - app
+The system cannot find the path specified.
+
+    C: \projects > npx create - react - app my - app
+Need to install the following packages:
+create - react - app
+Ok to proceed ? (y) y
+npm WARN deprecated tar @2.2.2: This version of tar is no longer supported, and will not receive security updates.Please upgrade asap.
+
+Creating a new React app in C: \projects\my - app.
+
+Installing packages.This might take a couple of minutes.
+Installing react, react - dom, and react - scripts with cra - template...
+
+
+added 1392 packages in 7m
+
+203 packages are looking for funding
+  run`npm fund` for details
+
+Initialized a git repository.
+
+Installing template dependencies using npm...
+npm WARN deprecated source - map - resolve@0.6.0: See https://github.com/lydell/source-map-resolve#deprecated
+
+added 52 packages in 25s
+
+203 packages are looking for funding
+  run`npm fund` for details
+Removing template package using npm...
+
+
+removed 1 package, and audited 1444 packages in 7s
+
+203 packages are looking for funding
+  run`npm fund` for details
+
+6 high severity vulnerabilities
+
+To address all issues(including breaking changes), run:
+  npm audit fix--force
+
+Run `npm audit` for details.
+
+Created git commit.
+
+        Success! Created my - app at C: \projects\my - app
+Inside that directory, you can run several commands:
+
+  npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory.If you do this, you can’t go back!
+
+We suggest that you begin by typing:
+
+  cd my - app
+  npm start
+
+Happy hacking!
+
+C: \projects > cd my - app
+
+C: \projects\my - app > npm start
+
+    > my - app@0.1.0 start
+        > react - scripts start
+
+            (node: 6316)[DEP_WEBPACK_DEV_SERVER_ON_AFTER_SETUP_MIDDLEWARE] DeprecationWarning: 'onAfterSetupMiddleware' option is deprecated.Please use the 'setupMiddlewares' option.
+(Use`node --trace-deprecation ...` to show where the warning was created)
+                (node: 6316)[DEP_WEBPACK_DEV_SERVER_ON_BEFORE_SETUP_MIDDLEWARE] DeprecationWarning: 'onBeforeSetupMiddleware' option is deprecated.Please use the 'setupMiddlewares' option.
+Starting the development server...
+Compiled successfully!
+
+You can now view my - app in the browser.
+
+    Local: http://localhost:3000
+  On Your Network: http://192.168.0.102:3000
+
+Note that the development build is not optimized.
+To create a production build, use npm run build.
+
+webpack compiled successfully
+
+
+
+//stop korte chaile control + c
+//yes/no te y dibo
+//same naam diye abr projects folder e add khula jabe na like
+// npx create-react-app my-app
+//my-app er place e my-app2 dibo
+//ager file ta delete kore dileo hbe
+//ager directory te jete hole cd.. dibo
---
+++ quiz.js
@@ -0,0 +1,21 @@
+The Keyboard Shortcut for clearing console in chrome dev tool is -
+
+    control + l
+
+
+What does SSL stand for?
+
+    secured socket layer
+
+
+What is a call stack ?
+    all previous function that called this current function
+
+
+    What is console ?
+        an object
+
+
+In which dev tool tab will you find the XHR in chrome ?
+    application
+
